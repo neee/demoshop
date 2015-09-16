3e95aa92e0f51dbe6e0a2988cdb71717b84921a7
neee <itneee@gmail.com>
2015-09-16 18:35:10 +0300

    sprepare sendClaim

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -125,6 +125,32 @@ function sendClaim(basket){
     //</specificationListRow>
     //</specificationList>
     //</inParams>"
+    var endpoint = "https://anketa.alfabank.ru/alfaform-pos/endpoint";
+    post(endpoint, {inXml:xml.documentElement.outerHTML});
+}
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+            form.setAttribute("target", "_blank");
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
 }
 
 $(document).ready(
