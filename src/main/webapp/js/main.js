138cee0619bbdb5d672373cf7974ff6cc2d26f90
neee <itneee@gmail.com>
2015-09-16 17:45:29 +0300

    sprepare sendClaim

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -49,6 +49,84 @@ function renderTable() {
     }
 }
 
+function sendClaim(basket){
+    var xml = document.implementation.createDocument('', '', null);
+    var inParams = xml.createElement("inParams");
+    //companyInfp
+    var companyInfo =  xml.createElement("companyInfo");
+        var inn = xml.createElement("inn");
+        inn.innerHTML = "7777777";
+    companyInfo.appendChild(inn);
+    //creditInfo
+    var creditInfo = xml.createElement("creditInfo");
+        var reference = xml.createElement("reference");
+        var firstPayment = xml.createElement("firstPayment");
+        var creditPeriod = xml.createElement("creditPeriod");
+        var creditProductCode = xml.createElement("creditProductCode");
+        var shopCode = xml.createElement("shopCode");
+    creditInfo.appendChild(reference);
+    creditInfo.appendChild(firstPayment);
+    creditInfo.appendChild(creditPeriod);
+    creditInfo.appendChild(creditProductCode);
+    creditInfo.appendChild(shopCode);
+    //clientInfo
+    var clientInfo = xml.createElement("clientInfo");
+        var lastname = xml.createElement("lastname");
+        var firstname = xml.createElement("firstname");
+        var middlename = xml.createElement("middlename");
+        var passportNamber = xml.createElement("passportNamber");
+        var passportSeries = xml.createElement("passportSeries");
+        var email = xml.createElement("email");
+        var mobphone = xml.createElement("mobphone");
+    clientInfo.appendChild(lastname);
+    clientInfo.appendChild(firstname);
+    clientInfo.appendChild(middlename);
+    clientInfo.appendChild(passportNamber);
+    clientInfo.appendChild(passportSeries);
+    clientInfo.appendChild(email);
+    clientInfo.appendChild(mobphone);
+    //specificationList
+    var specificationList = xml.createElement("specificationList");
+    if (!$.isEmptyObject(shopBasket)){
+        for(var item in shopBasket){
+            var row = xml.createElement("specificationListRow");
+            var desc = xml.createElement("description");
+            desc.innerHTML = item;
+            var amount = xml.createElement("amount");
+            amount.innerHTML = shopBasket[item].count;
+            var price = xml.createElement("price");
+            price.innerHTML = shopBasket[item].cost;
+            row.appendChild(desc);
+            row.appendChild(amount);
+            row.appendChild(price);
+            specificationList.appendChild(row);
+        }
+    }
+
+    inParams.appendChild(companyInfo);
+    inParams.appendChild(creditInfo);
+    inParams.appendChild(clientInfo);
+    inParams.appendChild(specificationList);
+    xml.appendChild(inParams);
+    //<inParams>
+    //    <companyInfo><inn>7804402344</inn></companyInfo>
+    //    <creditInfo><reference>A0000000001</reference><firstPayment>0</firstPayment><creditPeriod>15</creditPeriod>
+    //      <creditProductCode>JLAO</creditProductCode><shopCode>ALCT133_IS</shopCode>
+    //    </creditInfo>
+    //    <clientInfo><lastname>Арсентьев</lastname><firstname>Антон</firstname><middlename>Андреевич</middlename>
+    //      <passportNumber>1212</passportNumber><passportSeries>123456</passportSeries><email>lol4e@gmail.com</email>
+    //      <mobphone>9857715151</mobphone>
+    //    </clientInfo>
+    //
+    //<specificationList><specificationListRow><category>CRT_TV</category><code>#123</code><description>Samsung</description>
+    //  <amount>1</amount><price>10000</price></specificationListRow>
+    //<specificationListRow><category>MOBILE_PHONE</category><code>#1222</code><description>HTC</description>
+    //  <amount>2</amount><price>10000</price>
+    //</specificationListRow>
+    //</specificationList>
+    //</inParams>"
+}
+
 $(document).ready(
     function () {
         var buttonsBuy = document.querySelectorAll('.addItem');
